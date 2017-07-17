f5ae53eeab15c86837cc0bd0f08783784e5fa1d9
Varun Davda <varundavda006@gmail.com>
2017-07-17 11:54:47 +0530

    Removed navigation menu

--- resources/views/layouts/public/header.blade.php
+++ resources/views/layouts/public/header.blade.php
@@ -1,24 +1,23 @@
-<header id="header" class="transparent-header page-section dark">
+{{--<header id="header" class="transparent-header page-section dark">--}}
 
-    <div id="header-wrap">
+    {{--<div id="header-wrap">--}}
 
-        <div class="container clearfix">
+        {{--<div class="container clearfix">--}}
 
-            <div id="primary-menu-trigger"><i class="icon-reorder"></i></div>
+            {{--<div id="primary-menu-trigger"><i class="icon-reorder"></i></div>--}}
 
             <!-- Logo
             ============================================= -->
-            <div id="logo">
+            {{--<div id="logo">--}}
                 {{--<a href="/" class="standard-logo" data-dark-logo="images/logo-dark.png"><img src="images/logo.png" alt="Canvas Logo"></a>--}}
                 {{--<a href="/" class="retina-logo" data-dark-logo="images/logo-dark@2x.png"><img src="images/logo@2x.png" alt="Canvas Logo"></a>--}}
-            </div><!-- #logo end -->
+            {{--</div><!-- #logo end -->--}}
 
             <!-- Primary Navigation
             ============================================= -->
-            @include('layouts.public.nav')
+            {{--@include('layouts.public.nav')--}}
 
-        </div>
+        {{--</div>--}}
 
-    </div>
-
-</header><!-- #header end -->
+    {{--</div>--}}
+    <!--  </header>#header end -->
